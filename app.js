133d527a14fc9408ff627a1c593d9a1ea4714683
aszad007 <mohammadaszad13@gmail.com>
2022-09-11 15:20:06 +0530

    SIXTH-push

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+// document.getElementById('quote').innerHTML = quote;
+// document.getElementById('author').innerHTML = author;
+const text = document.querySelector('.quote');
+const author = document.querySelector('.author');
+
 const getQuote = async () => {
     const res = await fetch('https://type.fit/api/quotes');
-    const quote = await res.json();
-    const num = Math.floor(Math.random()*quote.length);
-    console.log(num);
+    const quotes = await res.json();
+    const num = Math.floor(Math.random()*quotes.length);
+    const item = quotes[num]; 
+    const quote = item.text;
+    const authorName = item.author;
+    text.innerText = quote;
+    author.innerText = authorName;
 }
 getQuote()
